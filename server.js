69ed39ba4f76d0979c7cfeb93411bf0329980f93
ahmedjw <ahmedvander@gmail.com>
2022-08-29 09:07:34 +0300

    Connect with mongodb

--- server.js
+++ server.js
@@ -1,26 +1,38 @@
 const express = require("express");
 const app = express();
-var http = require("http").Server(app);
-var io = require("socket.io")(http);
-let messages = [{ name: "ahmed", message: "Hi" }];
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.post("/messages", (req, res) => {
-  messages.push(req.body);
-  io.emit("message", () => req.body);
-  res.send(200);
-});
-app.get("/messages", (req, res) => {
-  res.send(messages);
+const http = require("http");
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+const mongoose = require("mongoose");
+mongoose.connect(
+  "mongodb+srv://<User>:<Password>@cluster0.2johae9.mongodb.net/test"
+);
+
+let Message = mongoose.model("Message", {
+  name: String,
+  message: String,
 });
 app.use(express.static(__dirname));
-var server = http.listen(3000, () => {
-  console.log("server is listening on port", server.address().port);
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.post("/messages", (req, res) => {
+  let message = new Message(req.body);
+  message.save((err) => {
+    if (err) res.send(500);
+    io.emit("message", () => req.body);
+    res.send(200);
+  });
 });
-app.get("/users/:id", (req, res) => {
-  const userId = req.params.id;
-  res.send(userId);
+app.get("/messages", (req, res) => {
+  Message.find({}, (err, messages) => {
+    res.send(messages);
+  });
 });
 io.on("connection", (socket) => {
   console.log("a user connected");
+});
+server.listen(3000, () => {
+  console.log("server is listening on port", server.address().port);
 });
